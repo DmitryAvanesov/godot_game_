84e3058e0312e06a1ba65cd338965e15458b4dbb
Dima <dima.avanesov@yandex.ru>
2019-05-29 17:49:28 +0700

    Dmitry's test commit
    just to try

---
+++ Label.gd
@@ -0,0 +1,11 @@
+extends Label
+
+var myText = "Hello my nibbas"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = myText
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
