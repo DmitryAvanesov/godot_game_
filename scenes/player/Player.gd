04d04fdc835b565025c9aa0749269dcda99954c1
Dima <dima.avanesov@yandex.ru>
2019-06-02 19:57:13 +0700

    Animation, climbing

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -3,26 +3,65 @@ extends KinematicBody2D
 var velocity = Vector2()
 const SPEED = 400
 const GRAVITY = 50
+const CLIMBING_SPEED = 30
+const CLIMBING_DURATION_UP = 115
+const CLIMBING_DURATION_SIDE = 80
+var isClimbing = false
+var climbingTimerUp
+var climbingTimerSide
+var climbingDirection
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
-	# --- basic movement ---
-	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		$PlayerSprite.flip_h = false
+	if !isClimbing:	
+		# --- basic movement ---
 		
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-		$PlayerSprite.flip_h = true
-	
-	else:
-		velocity.x = 0
+		if Input.is_action_pressed("ui_right"):
+			velocity.x = SPEED
+			$PlayerAnimatedSprite.flip_h = false
+			$PlayerAnimatedSprite.play("walking")
+			
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = -SPEED
+			$PlayerAnimatedSprite.flip_h = true
+			$PlayerAnimatedSprite.play("walking")
 		
-	# --- gravity ---
-	
-	if is_on_floor():
-		velocity.y = 0
+		else:
+			velocity.x = 0
+			$PlayerAnimatedSprite.play("standing")
+			
+		# --- gravity ---
+		
+		if is_on_floor():
+			velocity.y = 0
+		else:
+			velocity.y += GRAVITY
+			
+		# --- climbing ---
+		
+		if is_on_wall() && Input.is_action_just_pressed("ui_climb"):
+			isClimbing = true
+			climbingTimerUp = CLIMBING_DURATION_UP
+			climbingTimerSide = CLIMBING_DURATION_SIDE
+			if $PlayerAnimatedSprite.flip_h == false:
+				climbingDirection = 1
+			else:
+				climbingDirection = -1
+			
 	else:
-		velocity.y += GRAVITY
+		# --- climbing ---
+		
+		if climbingTimerUp > 0:			
+			climbingTimerUp -= 1
+			velocity.x = 0
+			velocity.y = -CLIMBING_SPEED
+			move_and_slide(velocity)
+		else:
+			if climbingTimerSide > 0:
+				climbingTimerSide -= 1
+				velocity.x = climbingDirection * CLIMBING_SPEED
+				velocity.y = 0
+				move_and_slide(velocity)
+			else:
+				isClimbing = false
--- scenes/test_scene/enemy.gd
+++ scenes/test_scene/enemy.gd
@@ -19,7 +19,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 	
-#	if is_on_floor():
-#		velocity.y = 0
-#	else:
-#		velocity.y += GRAVITY
+	#if is_on_floor():
+	#	velocity.y = 0
+	#else:
+	#	velocity.y += GRAVITY
