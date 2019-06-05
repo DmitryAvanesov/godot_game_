4878be30d81ab5853d040953148ec152d0bd76e2
Dima <dima.avanesov@yandex.ru>
2019-06-05 20:00:10 +0700

    Animation [ in progress ]

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,42 +1,42 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-const SPEED = 400
-const GRAVITY = 50
-const CLIMBING_SPEED = 30
-const CLIMBING_DURATION_UP = 112
-const CLIMBING_DURATION_SIDE = 80
+var tmpVelocity
+const WALKING_SPEED = 300
+const GRAVITY = 20
+const CLIMBING_SPEED_UP = 200
+const CLIMBING_SPEED_SIDE = 100
+const CLIMBING_DURATION_UP = 35
+const CLIMBING_DURATION_SIDE = 15
 var isClimbing = false
 var climbingTimerUp
 var climbingTimerSide
 var climbingDirection
 
 func _physics_process(delta):
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
 	if !isClimbing:	
 		# --- basic movement ---
 		
-		if Input.is_action_pressed("ui_right"):
-			velocity.x = SPEED
-			$PlayerAnimatedSprite.flip_h = false
-			$PlayerAnimatedSprite.play("walking")
+		if Input.is_action_pressed("ui_right") && !is_on_wall():
+			velocity.x = 1
+			$PlayerSprite.flip_h = false
+			$PlayerSprite/AnimationPlayer.play("walking")
 			
-		elif Input.is_action_pressed("ui_left"):
-			velocity.x = -SPEED
-			$PlayerAnimatedSprite.flip_h = true
-			$PlayerAnimatedSprite.play("walking")
+		elif Input.is_action_pressed("ui_left") && !is_on_wall():
+			velocity.x = -1
+			$PlayerSprite.flip_h = true
+			$PlayerSprite/AnimationPlayer.play("walking")
 		
 		else:
 			velocity.x = 0
-			$PlayerAnimatedSprite.play("standing")
+			$PlayerSprite/AnimationPlayer.play("standing")
 			
 		# --- gravity ---
 		
 		if is_on_floor():
 			velocity.y = 0
-		else:
-			velocity.y += GRAVITY
+		elif !isClimbing:
+			velocity.y += 1
 			
 		# --- climbing ---
 		
@@ -44,7 +44,7 @@ func _physics_process(delta):
 			isClimbing = true
 			climbingTimerUp = CLIMBING_DURATION_UP
 			climbingTimerSide = CLIMBING_DURATION_SIDE
-			if $PlayerAnimatedSprite.flip_h == false:
+			if $PlayerSprite.flip_h == false:
 				climbingDirection = 1
 			else:
 				climbingDirection = -1
@@ -55,13 +55,34 @@ func _physics_process(delta):
 		if climbingTimerUp > 0:			
 			climbingTimerUp -= 1
 			velocity.x = 0
-			velocity.y = -CLIMBING_SPEED
-			move_and_slide(velocity)
+			velocity.y = -1
+		elif climbingTimerSide > 0:
+			climbingTimerSide -= 1
+			velocity.x = climbingDirection
+			velocity.y = 0
 		else:
-			if climbingTimerSide > 0:
-				climbingTimerSide -= 1
-				velocity.x = climbingDirection * CLIMBING_SPEED
-				velocity.y = 0
-				move_and_slide(velocity)
-			else:
-				isClimbing = false
+			isClimbing = false
+	
+	# --- movement itself ---
+	
+	tmpVelocity = velocity
+	tmpVelocity.y = 0
+	for i in range(WALKING_SPEED):
+		move_and_slide(tmpVelocity, Vector2(0, -1))
+	
+	if isClimbing:
+		if climbingTimerUp > 0:
+			tmpVelocity = velocity
+			tmpVelocity.x = 0
+			for i in range(CLIMBING_SPEED_UP):
+				move_and_slide(tmpVelocity, Vector2(0, -1))
+		else:
+			tmpVelocity = velocity
+			tmpVelocity.y = 0
+			for i in range(CLIMBING_SPEED_SIDE):
+				move_and_slide(tmpVelocity, Vector2(0, -1))
+	else:
+		tmpVelocity = velocity
+		tmpVelocity.x = 0
+		for i in range(GRAVITY):
+			move_and_slide(tmpVelocity, Vector2(0, -1))
