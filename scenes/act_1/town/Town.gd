2b8d5cb38cfac2c4719b2825a0e24973e429f055
ectuser <m.ectuser@gmail.com>
2019-06-27 18:57:51 +0700

    some labels

--- scenes/act_1/town/Town.gd
+++ scenes/act_1/town/Town.gd
@@ -2,6 +2,10 @@ extends "res://scenes/act_1/FuncLoadScene.gd"
 
 var dialog_timer = 0
 
+func turn_labels_off():
+	$Label.visible = false
+	$Label2.visible = false
+
 # the limits for the current scene	
 func setLimits():
 	GLOBAL.sceneScaleCoef = $Player.scale.x
@@ -22,7 +26,14 @@ func setLimits():
 			
 func _ready():
 	setLimits()
+	turn_labels_off()
 	GLOBAL.dialog_counter += 1
 func _physics_process(delta):
 	GLOBAL.scene = "town"
-	dialog_timer += 1
+	if (!$Player/PlayerSprite.flip_h && GLOBAL.is_player_moving):
+		dialog_timer += 1
+	if (dialog_timer >= 0 && dialog_timer < 150):
+		$Label.visible = true
+	elif (dialog_timer >= 150 && dialog_timer < 250):
+		$Label.visible = false
+		$Label2.visible = true
